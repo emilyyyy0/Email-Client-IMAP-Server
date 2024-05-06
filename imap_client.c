0047e61c642c5ddb7b57b8a46b2a3c6873fd8a31
Emily Gong <ehgong@student.unimelb.edu.au>
2024-05-06 12:15:01 +1000

    Added in Task 2.2 Notes and pdf

--- imap_client.c
+++ imap_client.c
@@ -7,7 +7,11 @@
 // Functions to log in, select folder, fetch messages, and other IMAP commands.
 // Handle IMAP protocol specifics like tag generation and response parsing.
 
-int create_connection() {
+int create_connection(const char *hostname, const char *port) {
+    struct addrinfo hints, *res, *res0; 
+
     printf("The connection function\n\n\n\n\n");
     return 1; 
 }
+
+// const char *hostname, const char *port
--- imap_client.h
+++ imap_client.h
@@ -1,6 +1,12 @@
 #ifndef IMAP_CLIENT_H
 #define IMAP_CLIENT_H // this is changed in my own code
 
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+
 // Struct to store the command line arguments
 typedef struct fetch_mail {
         char *username; 
